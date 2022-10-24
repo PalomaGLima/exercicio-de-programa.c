a78b15d8466d06676d886f267a0a4e91e7cd04db
Paloma <palomalima272@gmail.com>
2022-10-24 20:09:05 -0300

    02

---
+++ 02.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n, ant, suc;
+
+    printf("digite um numero\n");
+    scanf("%d", &n);
+
+    ant= n - 1;
+    suc= n + 1;
+
+    printf("antecessor e sucessor %d e %d\n", ant, suc);
+
+    return 0;
+}
