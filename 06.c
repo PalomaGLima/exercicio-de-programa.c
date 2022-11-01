1bc7aa2d6532e09d7b713a41fb02db4c23f367ed
Paloma <palomalima272@gmail.com>
2022-11-01 16:25:47 -0300

    06

---
+++ 06.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    
+    int h1, h2, m1, m2, oldh, youh, oldm, youm, soma, produto;
+do{
+    printf("digite a idade de dois homens\n");
+    scanf("%d %d", &h1, &h2);
+} while (h1 == h2);
+
+do{
+    printf("digite a idade de duas mulheres\n");
+    scanf("%d %d", &m1, &m2);
+} while (m1 == m2);
+    
+
+    if (h1 > h2)
+    {
+        oldh = h1;
+    }
+    else{
+        oldh = h2;
+    }
+    if (h1 < h2)
+    {
+        youh = h1;
+    }
+    else{
+        youh = h2;
+    }
+    
+    if (m1 > m2){
+        oldm = m1;
+    }
+    else{
+        oldm = m2;
+    }
+    if (m1 < m2)
+    {
+        youm = m1;
+    }
+    else {
+        youm = m2;
+    }
+
+    soma = oldh + youm;
+    produto = oldm * youh;
+
+    printf("soma das idades:%d\n", soma);
+    printf("produto das idades:%d\n", produto);
+
+    return 0;
+}
